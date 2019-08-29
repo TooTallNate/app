efede09c2d64504ca78fdba865b8e1655013ffdb
Adrian Rocke <me@adrianrocke.com>
2019-08-29 11:31:08 -0500

    authenticate using credentials stored in session

--- api/index.js
+++ api/index.js
@@ -1,7 +1,75 @@
+const path = require("path");
+require("dotenv").config(path.resolve(__dirname, "../.env"));
 const app = require("express")();
-require("../src/setupProxy")(app);
+const request = require("httpntlm");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const uuid = require("uuid/v4");
+const FileStore = require("session-file-store")(session);
 
-const port = process.env.PORT;
+passport.use(
+  new LocalStrategy((domainUsername, password, done) => {
+    // Get the user info from NAV. If we succeed, the user is logged in.
+    const [domain, username] = domainUsername.split("\\");
+    const url = `${process.env.NAV_BASE_URL}/User?$filter=User_Name eq '${domainUsername}'`;
+    console.log(url);
+    request.get({ url, username, password, domain }, (err, result) => {
+      if (err) return done(err);
+      done(null, { ...JSON.parse(result.body).value[0], password });
+    });
+  })
+);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(
+  session({
+    genid: uuid,
+    store: new FileStore(),
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post("/api/login", passport.authenticate("local"), (req, res) => {
+  res.status(204).end();
+});
+
+app.post("/api/logout", (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    else res.status(204).end();
+  });
+});
+
+// Proxy requests to the NAV server using the auth information in the session.
+app.use("/api", (req, res, next) => {
+  const [domain, username] = req.user.User_Name.split("\\");
+  request.get(
+    {
+      url: `${process.env.NAV_BASE_URL}/${req.url}`,
+      domain,
+      username,
+      password: req.user.password
+    },
+    (err, result) => {
+      res.status(result.statusCode).send(result.body);
+    }
+  );
+});
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`proxy listening on port ${port}`));
 
 module.exports = app;
--- src/service.ts
+++ src/service.ts
@@ -1,3 +1,30 @@
+async function login({
+  username,
+  password
+}: {
+  username: string;
+  password: string;
+}): Promise<boolean> {
+  const url = `/api/login`;
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username,
+      password
+    })
+  });
+  return response.status === 204;
+}
+
+async function logout() {
+  const url = `/api/logout`;
+  const response = await fetch(url, { method: "POST" });
+  return response.status === 204;
+}
+
 async function getJobList() {
   const url = `/api/Job_List?%24filter=Status%20eq%20'Open'&%24format=json&%24select=No`;
   const response = await fetch(url, {
@@ -7,4 +34,4 @@ async function getJobList() {
   return data.value.map(job => job.No);
 }
 
-export default { getJobList };
+export default { login, logout, getJobList };
