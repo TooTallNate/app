dcbb5c3a87387deb142fa4d47dfebcee7a0adccf
Adrian Rocke <me@adrianrocke.com>
2019-11-19 16:03:03 -0600

    fix flash ts issues

--- web/src/components/Flash.tsx
+++ web/src/components/Flash.tsx
@@ -17,26 +17,24 @@ interface FlashProps {
 }
 
 const Flash: React.FC<FlashProps> = ({ messages, onClose }) => {
-  const [currentMessage, setCurrentMessage] = useState<FlashMessage>();
+  const [msg, setMsg] = useState<FlashMessage>();
   const [isOpen, setIsOpen] = useState(false);
-
-  const { message: text, level, timeout } = currentMessage || {};
 
   // Set the current message and visibility whenever the first message is new.
   useEffect(() => {
-    if (currentMessage !== messages[0]) {
-      setCurrentMessage(messages[0]);
+    if (msg !== messages[0]) {
+      setMsg(messages[0]);
       setIsOpen(messages.length > 0);
     }
-  }, [currentMessage, messages]);
+  }, [msg, messages]);
 
   // Enable the timeout if set.
   useEffect(() => {
-    if (typeof timeout !== "undefined") {
-      const id = setTimeout(() => setIsOpen(false), timeout);
+    if (msg && typeof msg.timeout !== "undefined") {
+      const id = setTimeout(() => setIsOpen(false), msg.timeout);
       return () => clearTimeout(id);
     }
-  }, [timeout]);
+  }, [msg]);
 
   return (
     <div css={tw`fixed top-0 left-0 right-0 flex justify-center`}>
@@ -52,24 +50,25 @@ const Flash: React.FC<FlashProps> = ({ messages, onClose }) => {
           data-testid="flash-message"
           css={[
             tw`max-w-2xl w-full px-3 py-2 mt-4 shadow rounded flex`,
-            level === "error" && tw`bg-red-400`,
-            level === "warn" && tw`bg-orange-300`,
-            level === "info" && tw`bg-blue-300`,
-            level === "success" && tw`bg-green-400`
+            msg && msg.level === "error" && tw`bg-red-400`,
+            msg && msg.level === "warn" && tw`bg-orange-300`,
+            msg && msg.level === "info" && tw`bg-blue-300`,
+            msg && msg.level === "success" && tw`bg-green-400`
           ]}
         >
           <FontAwesomeIcon
             css={tw`mt-1`}
             icon={
-              (level === "error" && "times-circle") ||
-              (level === "warn" && "exclamation-circle") ||
-              (level === "info" && "info-circle") ||
-              (level === "success" && "check-circle") ||
+              (msg &&
+                ((msg.level === "error" && "times-circle") ||
+                  (msg.level === "warn" && "exclamation-circle") ||
+                  (msg.level === "info" && "info-circle") ||
+                  (msg.level === "success" && "check-circle"))) ||
               "info-circle"
             }
           />
           <div css={tw`ml-2 mr-4 flex-1`} role="alert">
-            {text || ""}
+            {msg && msg.message}
           </div>
           <button
             aria-label="Close Flash Message"
