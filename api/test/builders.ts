8fddf518fc953818e75f7258db6dfb5686dcf6d1
Adrian Rocke <me@adrianrocke.com>
2020-02-11 16:43:13 -0600

    test utils

--- api/test/builders.ts
+++ api/test/builders.ts
@@ -1,34 +1,15 @@
 import faker from "faker";
 import * as Factory from "factory.ts";
-import { NavJob, NavUser, NavDimension } from "../nav/types";
+import { NavUser } from "../nav/types";
 import uuid from "uuid/v4";
 
-const JOB_POSTING_GROUPS = ["MKT PIGS", "GDU", "SOWS", "CATTLE", "FARROW-BE"];
-const JOB_STATUSES = ["Planning", "Open", "Complete"];
-const DIMENSION_CODES = ["ENTITY", "COST CENTER"];
+function oneOf<T>(...list: T[]) {
+  return list[faker.random.number({ min: 0, max: list.length - 1 })];
+}
 
-const oneOf = (list: any[]): (() => any) => () =>
-  list[faker.random.number({ min: 0, max: list.length - 1 })];
-
-export const userFactory = Factory.Sync.makeFactory<NavUser>({
+export const UserFactory = Factory.Sync.makeFactory<NavUser>({
   User_Security_ID: Factory.each(() => uuid()),
   Full_Name: Factory.each(() => faker.name.findName()),
-  License_Type: "Full User"
-});
-
-export const jobFactory = Factory.Sync.makeFactory<NavJob>({
-  No: Factory.each(() => faker.random.alphaNumeric(8)),
-  Job_Posting_Group: Factory.each(oneOf(JOB_POSTING_GROUPS)),
-  Site: Factory.each(() => faker.random.number({ min: 1, max: 99 }).toString()),
-  Status: Factory.each(oneOf(JOB_STATUSES))
-});
-
-export const dimensionFactory = Factory.Sync.makeFactory<NavDimension>({
-  Table_ID: 167,
-  No: Factory.each(() => faker.random.alphaNumeric(8)),
-  Dimension_Code: Factory.each(oneOf(DIMENSION_CODES)),
-  Dimension_Value_Code: Factory.each(() =>
-    faker.random.number({ min: 1, max: 500 }).toString()
-  ),
-  Value_Posting: "Code Mandatory"
+  License_Type: Factory.each(() => oneOf("Full User", "Limited User")),
+  User_Name: Factory.each(() => faker.internet.userName())
 });
--- api/test/utils.ts
+++ api/test/utils.ts
@@ -1,8 +1,4 @@
-import { GraphQLClient, ClientError } from "graphql-request";
-import { GraphQLError } from "graphql-request/dist/src/types";
-import { User, MutationLoginArgs } from "../resolvers/types";
-import { mock } from "../nav";
-import { NavMock } from "../nav/__mocks__";
+import { GraphQLClient } from "graphql-request";
 
 const port = process.env.PORT;
 
@@ -17,53 +13,4 @@ declare global {
 
 global.fetch = require("fetch-cookie/node-fetch")(require("node-fetch"));
 
-const client = new GraphQLClient(`http://localhost:${port}`);
-
-const navMock = mock as NavMock;
-export { navMock };
-
-interface GQLResponse<T> {
-  data?: T;
-  extensions?: any;
-  headers: Headers;
-  status: number;
-  errors?: GraphQLError[];
-}
-
-export function gql<T, V = void>(
-  query: string
-): (variables?: V) => Promise<GQLResponse<T>> {
-  return async (variables: V) => {
-    try {
-      return await client.rawRequest<T>(query, variables);
-    } catch (err) {
-      const response = (err as ClientError).response;
-      if (response) return response as GQLResponse<T>;
-      else throw err;
-    }
-  };
-}
-
-export async function login() {
-  const { ntPassword, lmPassword, ...creds } = navMock.credentials;
-  const { errors } = await gql<{ login: User }, MutationLoginArgs>(
-    `mutation Login($input: LoginInput!) {
-      login(input: $input) {
-        name
-        license
-      }
-    }`
-  )({ input: creds });
-  expect(errors).toBeFalsy();
-}
-
-export async function expectUnauthorized(
-  fn: () => Promise<{ errors?: { message: string }[] }>
-): Promise<void> {
-  const { errors } = await fn();
-  expect(errors).toEqual([
-    expect.objectContaining({
-      message: "Unauthorized"
-    })
-  ]);
-}
+export const client = new GraphQLClient(`http://localhost:${port}`);
