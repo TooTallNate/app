2e39cbc44d32eed107d55873e3223272264fd0b8
matt <mwoelfel13@gmail.com>
2020-11-27 12:28:31 -0600

    added save button and functionality for scorecardpages

--- web/src/scorecard/views/nursery-finisher/ScorecardPageView.tsx
+++ web/src/scorecard/views/nursery-finisher/ScorecardPageView.tsx
@@ -10,6 +10,10 @@ import FormSubmit from "../../../common/components/form/FormSubmit";
 import ViewContent from "../../../common/components/view/ViewContent";
 import { useGrowFinish } from "../../contexts/growFinish";
 import ScorecardPageComponent from "../../components/ScorecardPageComponent";
+import Button from "../../../common/components/input/Button";
+import HorizontalSpacer from "../../../common/components/layout/HorizontalSpacer";
+import { useFlash } from "../../../common/contexts/flash";
+import { useSaveScorecardMutation } from "../../graphql";
 
 const ScorecardPageView: React.FC = () => {
   const params = useParams<{ page: string; job: string }>();
@@ -25,6 +29,9 @@ const ScorecardPageView: React.FC = () => {
   } = useGrowFinish();
   const formContext = useForm();
   const { setValue } = formContext;
+  const { setMessage } = useFlash();
+
+  const [save] = useSaveScorecardMutation();
 
   // Refresh job on page reload.
   useEffect(() => {
@@ -49,6 +56,33 @@ const ScorecardPageView: React.FC = () => {
     }
   };
 
+  const onSave = async () => {
+    try {
+      await save({
+        variables: {
+          input: {
+            job: params.job,
+            data: Object.entries(formState).map(([key, value]) => ({
+              elementId: key,
+              ...value
+            }))
+          }
+        }
+      });
+      setMessage({
+        message: "Entry saved successfully.",
+        level: "success",
+        timeout: 2000
+      });
+      history.push("/");
+    } catch (e) {
+      setMessage({
+        message: e.toString(),
+        level: "error"
+      });
+    }
+  };
+
   return (
     <View>
       <ViewHeader>
@@ -63,7 +97,13 @@ const ScorecardPageView: React.FC = () => {
                 <ScorecardPageComponent key={element.id} {...element} />
               ))}
             <div className="flex-grow" />
-            <FormSubmit>Continue</FormSubmit>
+            <div className="flex">
+              <Button className="w-full" type="button" onClick={onSave}>
+                Save
+              </Button>
+              <HorizontalSpacer />
+              <FormSubmit>Continue</FormSubmit>
+            </div>
           </Form>
         )}
       </ViewContent>
