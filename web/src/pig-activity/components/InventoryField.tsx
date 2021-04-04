6a6ab6e0a7e883541e9699f09f3cc5638b88a457
sergeihanka <sergeiahanka@gmail.com>
2021-04-03 20:39:37 -0500

    ui hotfix

--- web/src/pig-activity/components/InventoryField.tsx
+++ web/src/pig-activity/components/InventoryField.tsx
@@ -1,58 +1,55 @@
 import React from "react";
 import FormField from "../../common/components/form/FormField";
-import FormFieldLabel from "../../common/components/form/FormFieldLabel";
 import FormFieldInput from "../../common/components/form/FormFieldInput";
+import FormFieldLabel from "../../common/components/form/FormFieldLabel";
 import StaticValue from "../../common/components/input/StaticValue";
+import Divider from "../../common/components/layout/Divider";
+
 export interface InventoryFieldProps {
   className?: string;
   inventory: number;
   deadQuantity: number;
 }
-const TextRow = (props: {
-  className: string | undefined;
-  label: string;
-  value: string | number;
-}) => (
-  <FormField name="inventory" className={props.className}>
-    <div className="flex w-full items-center divide-y divide-gray-200">
-      <div className="flex w-48 items-start">
-        <FormFieldLabel>{props.label}</FormFieldLabel>
-      </div>
-      <div style={{ backgroundColor: "blue" }}>
-        <FormFieldInput className="flex" noRegister>
-          <StaticValue
-            className="flex items-center content-center justify-items-center"
-            value={`${props.value}`}
-          />
-        </FormFieldInput>
-      </div>
-    </div>
-  </FormField>
-);
 const InventoryField: React.FC<InventoryFieldProps> = ({
   className,
   inventory,
   deadQuantity
 }) => {
   return (
-    <div>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300"></div>
+    <div className="py-2">
+      <Divider />
+      <FormField name="inventory" className={className}>
+        <div className="flex w-full items-center divide-y divide-gray-200">
+          <div className="flex w-48 items-start">
+            <FormFieldLabel>Inventory</FormFieldLabel>
+          </div>
+          <div>
+            <FormFieldInput className="flex" noRegister>
+              <StaticValue
+                className="flex items-center content-center justify-items-center"
+                value={`${inventory}`}
+              />
+            </FormFieldInput>
+          </div>
         </div>
-      </div>
-      <TextRow className={className} label="Inventory" value={`${inventory}`} />
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300"></div>
+      </FormField>
+      <Divider />
+      <FormField name="inventory" className={className}>
+        <div className="flex w-full items-center divide-y divide-gray-200">
+          <div className="flex w-48 items-start">
+            <FormFieldLabel>Deads</FormFieldLabel>
+          </div>
+          <div>
+            <FormFieldInput className="flex" noRegister>
+              <StaticValue
+                className="flex items-center content-center justify-items-center"
+                value={`${deadQuantity}`}
+              />
+            </FormFieldInput>
+          </div>
         </div>
-      </div>
-      <TextRow className={className} label="Deads" value={`${deadQuantity}`} />
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center" aria-hidden="true">
-          <div className="w-full border-t border-gray-300"></div>
-        </div>
-      </div>
+      </FormField>
+      <Divider />
     </div>
   );
 };
