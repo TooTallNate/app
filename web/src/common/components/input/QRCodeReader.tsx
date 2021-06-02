404b77ad28752f72232c7733ae1731002deffa5e
sergeihanka <sergeiahanka@gmail.com>
2021-06-01 21:22:05 -0500

    updated overlay size

--- web/src/common/components/input/QRCodeReader.tsx
+++ web/src/common/components/input/QRCodeReader.tsx
@@ -16,7 +16,7 @@ const codeReader = new BrowserQRCodeReader();
 const QRCodeReader = () => {
   const { setMessage } = useFlash();
   const history = useHistory();
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const [deviceId, setDeviceId] = useState("");
 
   useEffect(() => {
@@ -73,6 +73,30 @@ const QRCodeReader = () => {
     setOpen(false);
   }
 
+  const ScannerHeader = () => (
+    <div className="flex items-start justify-between mb-6">
+      <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">
+        Scan QR Code
+      </h2>
+      <div className="ml-3 h-7 flex items-center">
+        <button
+          className="bg-white rounded-md text-gray-800 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          onClick={reset}
+        >
+          <span className="sr-only">Close panel</span>
+          <XIcon className="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+    </div>
+  );
+
+  const VideoRecorder = () => (
+    <video
+      id="video"
+      className="border-2 border-dashed border-gray-200 rounded-lg p-3"
+    />
+  );
+
   return (
     <div>
       <Button onClick={startDecode} className="w-full">
@@ -85,50 +109,22 @@ const QRCodeReader = () => {
         aria-modal="true"
         hidden={!open}
       >
-        <div className="absolute inset-0 overflow-hidden" onClick={reset}>
-          <div className="fixed inset-y-0 w-full flex">
-            <div className="w-screen">
-              <div className="h-full flex flex-col py-6 bg-white shadow-xl overflow-y-scroll rounded-t-lg">
-                <div className="px-4 sm:px-6">
-                  <div className="flex items-start justify-between">
-                    <h2
-                      className="text-lg font-medium text-gray-900"
-                      id="slide-over-title"
-                    >
-                      Scan QR Code
-                    </h2>
-                    <div className="ml-3 h-7 flex items-center">
-                      <button
-                        className="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        onClick={reset}
-                      >
-                        <span className="sr-only">Close panel</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div className="mt-6 relative flex-1 px-4 sm:px-6">
-                  {open && (
-                    <div
-                      className="border-2 p-3 border-dashed border-gray-200 justify-center"
-                      aria-hidden="true"
-                    >
-                      <video id="video" className="mx-auto" height="300" />
-                    </div>
-                  )}
-                </div>
-                <div className="flex-shrink-0 px-4 py-4 flex justify-center">
-                  <Button
-                    type="button"
-                    className="w-full max-w-xl bg-white border border-gray-300 text-red-600 hover:bg-gray-200"
-                    onClick={reset}
-                  >
-                    Cancel
-                  </Button>
-                </div>
-              </div>
-            </div>
+        <div className="h-full flex flex-col bg-white overflow-y-scroll p-5">
+          <h2 className="sr-only" id="profile-overview-title">
+            Scan QR Code
+          </h2>
+          <ScannerHeader />
+          <div className="flex justify-center align-center mt-auto mb-auto">
+            <VideoRecorder />
+          </div>
+          <div className="mt-auto">
+            <Button
+              type="button"
+              className="w-full max-w-xl bg-white border border-gray-300 text-red-600 hover:bg-gray-200"
+              onClick={reset}
+            >
+              Cancel
+            </Button>
           </div>
         </div>
       </div>
