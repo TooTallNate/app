3d9ab2429e66d42c7ea29c4734e47ecc359e0cce
Micah Mogler <micah.mogler@gmail.com>
2021-11-07 17:44:50 -0600

    changed filters from notEquals( to equals(

--- api/src/common/datasources/NavJobDataSource.ts
+++ api/src/common/datasources/NavJobDataSource.ts
@@ -108,9 +108,9 @@ export default class LivestockJobNavDataSource extends NavDataSource {
         );
       }
       if (isShipment) {
-        filters.push(f.notEquals("Barn_Type", "Finisher"));
-        filters.push(f.notEquals("Barn_Type", ""));
-        filters.push(f.notEquals("Barn_Type", " "));
+        filters.push(f.equals("Barn_Type", "Finisher"));
+        filters.push(f.equals("Barn_Type", ""));
+        filters.push(f.equals("Barn_Type", " "));
         filters.push(f.greaterThan("Inventory_Left", 0));
       }
       return f.and(...filters);
