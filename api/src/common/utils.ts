0d68b2dfe81332cb40a4bc6749a58b1f0f424956
Matt Woelfel <mwoelfel13@gmail.com>
2021-12-28 20:52:49 -0600

    Revert "fixing date posting to 2022"
    This reverts commit cdb2bf90925364d393ecabe02a5eef5ca54f7407.

--- api/src/common/utils.ts
+++ api/src/common/utils.ts
@@ -7,7 +7,7 @@ export function getDocumentNumber(form: string, username: string) {
 }
 
 export function navDate(date: Date) {
-  return format(date, "yyyy-MM-dd");
+  return format(date, "YYY-MM-dd");
 }
 
 export function parseNavDate(dateStr: string) {
@@ -40,6 +40,5 @@ export const MenuOptions = [
   { Name: "Livestock Activity", Route: "/livestock-activity" },
   { Name: "Scorecards", Route: "/scorecard" },
   { Name: "Fuel", Route: "/fuel" },
-  { Name: "Maintenance", Route: "/maintenance" },
-  { Name: "Inventory  Use", Route: "/inventory" }
+  { Name: "Maintenance", Route: "/maintenance" }
 ];
